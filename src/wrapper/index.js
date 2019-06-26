eea517e01a294f4adeac0558da38782158c69aa9
dhollenbeck <dan@hollenbecks.com>
2019-06-26 18:33:57 +0000

    bug fix

--- src/wrapper/index.js
+++ src/wrapper/index.js
@@ -7,9 +7,9 @@ module.exports = function Wrapper (opts) {
 	var source = opts.source;
 	var invoked = opts.invoked;
 
-	function toFrame(method, name, value) {
+	function toFrame(type, name, value) {
 		return new Frame({
-			method: method,
+			type: type,
 			source: source,
 			name: name,
 			value: value,
@@ -18,26 +18,26 @@ module.exports = function Wrapper (opts) {
 	}
 
 	function info(arg0, arg1) {
-		var method = 'info';
-		var frame = toFrame(method, arg0, arg1);
+		var type = 'info';
+		var frame = toFrame(type, arg0, arg1);
 		console.info(frame);
 	}
 
 	function warn(arg0, arg1) {
-		var method = 'warn';
-		var frame = toFrame(method, arg0, arg1);
+		var type = 'warn';
+		var frame = toFrame(type, arg0, arg1);
 		console.warn(frame);
 	}
 
 	function error(arg0, arg1) {
-		var method = 'error';
-		var frame = toFrame(method, arg0, arg1);
+		var type = 'error';
+		var frame = toFrame(type, arg0, arg1);
 		console.error(frame);
 	}
 
 	function data(arg0, arg1) {
-		var method = 'data';
-		var frame = toFrame(method, arg0, arg1);
+		var type = 'data';
+		var frame = toFrame(type, arg0, arg1);
 		console.info(frame);
 	}
 
--- test/frame.js
+++ test/frame.js
@@ -14,4 +14,10 @@ describe('Console.Frame(): ', function () {
 		Assert.equal(frame instanceof Konsole.Frame, true);
 		done();
 	});
+	it('should handle `data` frame', function (done) {
+		var frame = new Konsole.Frame({type: 'data'});
+		Assert.equal(frame instanceof Konsole.Frame, true);
+		Assert.equal(frame.type, 'data');
+		done();
+	});
 });
